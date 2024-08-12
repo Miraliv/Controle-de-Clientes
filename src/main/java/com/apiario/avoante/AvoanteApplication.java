9da5358944b67960f23eff75ce4e28573d37b5a2
Kauã Vilarim <kauavilarim@gmail.com>
2024-08-12 09:08:17 -0300

    Front-end
    Inserção do projeto Spring web com as funcionalidades front-end mapeado pela classe PageController

---
+++ src/main/java/com/apiario/avoante/AvoanteApplication.java
@@ -0,0 +1,13 @@
+package com.apiario.avoante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvoanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvoanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiario/avoante/AvoanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiario.avoante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvoanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
